af8cc6bd616d07450a45cd503c45bf3a310f9830
Andrei Laza <andrei@estudent.ro>
2016-02-20 16:06:08 +0200

    Minor cors fixes

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+require "rack/cors"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,8 +30,7 @@ module CahootsApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-
+    # Do not swallow errors in after_commit/after_rollback callbacks.    
     config.middleware.use Rack::Cors do
       allow do        
         origins '*'
