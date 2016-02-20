80c0e6a3a17e8272732c9e192572a15fc7488024
Andrei Laza <andrei@estudent.ro>
2016-02-20 19:34:39 +0200

    Adds room

--- app/controllers/v1/rooms_controller.rb
+++ app/controllers/v1/rooms_controller.rb
@@ -5,4 +5,35 @@ class V1::RoomsController < ApplicationController
   def index
     render json: 'asg', root: false
   end
+
+  def create
+    room = Room.new(room_params)
+
+    user = User.new()
+    user.name = params[:user_name]
+    user.auth_token = SecureRandom.base64(20)
+
+    if room.save
+      if user.save
+        room = ActiveSupport::JSON.decode(room.to_json)
+
+        room['user'] = ActiveSupport::JSON.decode(user.to_json)
+
+        render json: room, status: 201, root: false
+      else
+        render json: user.errors, status: 201, root: false
+      end      
+    else
+      render json: { errors: room.errors }, status: 422
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:user_name)
+    end
+
+    def room_params
+      params.permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
 
-    resources :rooms, :only => [:index]
+    resources :rooms, :only => [:index, :create]
     resources :users, :only => [:create]
 
     post 'pusher/auth', to: 'pusher#auth'
-  end    
+  end
   
 end
